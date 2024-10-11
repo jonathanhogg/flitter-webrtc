b0acff58119e09dee24ed58f57f014356ba0d54d
Jonathan Hogg <me@jonathanhogg.com>
2024-10-11 15:08:47 +0100

    Add accellerated YUV420p conversion

--- src/flitter_webrtc/shader.py
+++ src/flitter_webrtc/shader.py
@@ -5,16 +5,80 @@ Flitter WebRTC Plugin
 import asyncio
 
 import aiortc
+import array
 from av.video.reformatter import VideoReformatter
 from loguru import logger
+import moderngl
 
 from flitter.model import Vector, null
 from flitter.plugins import get_plugin
 from flitter.render.window import ProgramNode
+from flitter.render.window.glconstants import GL_FRAMEBUFFER_SRGB
 from flitter.render.window.target import RenderTarget
 
 
 Reformatter = VideoReformatter()
+
+
+class VideoConverter:
+    VERTEX_SOURCE = """
+in vec2 position;
+out vec2 coord;
+
+void main() {
+    gl_Position = vec4(position.x, -position.y, 0.0, 1.0);
+    coord = (position + 1.0) / 2.0;
+}
+    """
+
+    FRAGMENT_SOURCE = """
+in vec2 coord;
+out vec4 color;
+
+uniform sampler2D y;
+uniform sampler2D u;
+uniform sampler2D v;
+
+const vec3 offset = vec3(0.0627451, 0.5, 0.5);
+const float scale = 1.138393;
+const mat3 srgb = mat3(1.0, 1.0, 1.0, 0.0, -0.21482, 2.12798, 1.28033, -0.38059, 0.0);
+
+void main() {
+    vec3 yuv = vec3(texture(y, coord).r, texture(u, coord).r, texture(v, coord).r);
+    color = vec4(srgb * ((yuv - offset) * scale), 1.0);
+}
+"""
+
+    def __init__(self, glctx):
+        self.glctx = glctx
+        self.y = self.u = self.v = None
+        header = self.glctx.extra['HEADER']
+        self.program = self.glctx.program(vertex_shader=header + self.VERTEX_SOURCE, fragment_shader=header + self.FRAGMENT_SOURCE)
+        vertices = self.glctx.buffer(array.array('f', [-1, 1, -1, -1, 1, 1, 1, -1]))
+        self.rectangle = self.glctx.vertex_array(self.program, [(vertices, '2f', 'position')], mode=moderngl.TRIANGLE_STRIP)
+
+    def convert(self, frame, target):
+        assert frame.format.name == 'yuv420p'
+        if self.y is None:
+            width, height = frame.width, frame.height
+            self.y = self.glctx.texture((width, height), 1)
+            self.u = self.glctx.texture((width // 2, height // 2), 1)
+            self.v = self.glctx.texture((width // 2, height // 2), 1)
+        data = frame.to_ndarray().data
+        n = len(data)
+        self.y.write(data[:n * 2 // 3])
+        self.u.write(data[n * 2 // 3:n * 5 // 6])
+        self.v.write(data[n * 5 // 6:])
+        self.y.use(1)
+        self.program['y'] = 1
+        self.u.use(2)
+        self.program['u'] = 2
+        self.v.use(3)
+        self.program['v'] = 3
+        with target:
+            self.glctx.disable_direct(GL_FRAMEBUFFER_SRGB)
+            target.clear()
+            self.rectangle.render()
 
 
 class RenderTrack(aiortc.VideoStreamTrack):
@@ -108,14 +172,16 @@ class WebRTC(ProgramNode):
 
     async def consume_remote_track(self, track):
         try:
+            converter = VideoConverter(self.glctx)
             while True:
                 frame = await track.recv()
                 if self._remote_target is None:
                     self._remote_target = RenderTarget.get(self.glctx, frame.width, frame.height, 8, srgb=True)
-                data = await asyncio.to_thread(lambda f: Reformatter.reformat(f, format='rgba').to_ndarray()[::-1].copy().data, frame)
-                self._remote_target.texture.write(data)
+                converter.convert(frame, self._remote_target)
         except asyncio.CancelledError:
             pass
+        except Exception:
+            logger.exception("Unexpected error in video decode")
 
     async def create_peer_connection(self):
         if self._peer_connection is not None:
